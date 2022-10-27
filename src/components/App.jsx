085e4d79d39b10901e04c49bf1df208c135aee59
OlgaBorovik <jkz8686@gmail.com>
2022-10-27 21:13:17 +0300

    added form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,30 @@
+import { Component } from 'react'
 import {Searchbar} from './Searchbar/Searchbar'
 
-export const App = () => {
-  return (
+export class App extends Component {
+state = {
+        query: '',
+    }
+
+    // componentDidMount() {
+    //     fetch('https://pixabay.com/api/?q=cat&page=1&key=29953966-c475d5dff4ed5a25f1b37ba96&image_type=photo&orientation=horizontal&per_page=12')
+    //         .then(res => res.json())
+    //         .then(image => this.setState({image}))
+    //         .then(console.log(this.state.image))
+    // }
+  handleFormSubmit = query => {
+  console.log('query', query)
+}
+
+  render() {
+    return (
     <div>
-      <Searchbar />
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        <button type="button">Load more</button>
     </div>
   );
+  }
+    
+  
+  
 };
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -2,31 +2,39 @@ import React, {Component} from 'react'
 
 export class Searchbar extends Component {
     state = {
-        image: '',
+        query: '',
     }
 
-    componentDidMount() {
-        fetch('https://pixabay.com/api/?q=cat&page=1&key=29953966-c475d5dff4ed5a25f1b37ba96&image_type=photo&orientation=horizontal&per_page=12')
-            .then(res => res.json())
-            .then(image => this.setState({image}))
-            .then(console.log(this.state.image))
+    onInputChange = (e) => {
+        this.setState( {query: e.currentTarget.value.toLowerCase() })
+    }
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.props.onSubmit(this.state.query)
+        this.setState({query: ''})
+
     }
     render() {
         return (
             <div>
                 <header className="searchbar">
                     <form className="form">
-                    <button type="submit" className="button">
-                    <span className="button-label">Search</span>
-                    </button>
+                        <button type="submit"
+                            className="button"
+                            onSubmit={this.handleSubmit}>
+                        <span className="button-label">Search</span>
+                            
+                        </button>
 
-                    <input
-                    className="input"
-                    type="text"
-                    autoComplete="off"
-                    autoFocus
-                    placeholder="Search images and photos"
-                    />
+                        <input
+                        className="input"
+                        type="text"
+                        autoComplete="off"
+                        autoFocus
+                            placeholder="Search images and photos"
+                            value={this.state.query}
+                            onChange={this.onInputChange}
+                        />
                     </form>
                 </header>
             </div>
